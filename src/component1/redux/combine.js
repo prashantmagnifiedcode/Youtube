0ff11bbbff550dc027ae3a089b617e5210b49776
Prashant Srivastava <82441036+prashantmagnifiedcode@users.noreply.github.com>
2022-02-01 13:04:50 +0530

    delete

--- src/component1/redux/combine.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import Chat_status from  './chatbox_state'
-import Chat_status1 from  './chatbox_status1'
-import Chat_login_data from  './login_data'
-const rootreducer= combineReducers({Chat_status,Chat_status1,Chat_login_data})
-export default rootreducer
- 
--- src/component1/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./combine";
-const Store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-export default Store
